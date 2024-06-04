b0a7aa7d2633abca96b8b3ff8db4a068bceb5591
wmwmss <xu.han@rhsmith.umd.edu>
2024-06-04 14:12:57 -0400

    Added star tips

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,14 +8,26 @@
           round
           icon="star"
           aria-label="Menu"
-          @click="toggleLeftDrawer"
+          @click="showStarTips"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title> Quasar Enhanced Counter </q-toolbar-title>
       </q-toolbar>
     </q-header>
+
+    <q-dialog v-model="starTips" persistent>
+      <q-card class="text-white q-dialog-card">
+        <q-card-section class="text-center">
+          <h5 class="q-ma-sm">
+            Click and Hold the buttons or Click and Drag up and down to operate
+            the counter!
+          </h5>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Close" class="close-btn" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 
     <q-img src="background.jpg" class="fit absolute" />
 
@@ -25,6 +37,27 @@
   </q-layout>
 </template>
 
-<script>
-import { defineComponent, ref } from 'vue'
+<script setup>
+import { ref } from 'vue'
+
+const starTips = ref(false)
+
+const showStarTips = () => {
+  starTips.value = !starTips.value
+}
 </script>
+
+<style scoped>
+.q-dialog-card {
+  background-color: #292dae; /* Nice decent background color */
+  color: white;
+}
+
+.close-btn {
+  background-color: white;
+  color: var(--q-primary);
+  border: 1px solid var(--q-primary);
+  text-transform: none;
+  padding: 8px 16px;
+}
+</style>
